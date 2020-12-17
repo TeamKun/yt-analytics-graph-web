8e851e9afddb2aead47b7dbe962195746e78b607
Kamesuta <kamesuta@gmail.com>
2020-12-17 10:53:33 +0900

    #

--- src/store/auths.ts
+++ src/store/auths.ts
@@ -2,7 +2,7 @@ import { Module, VuexModule, Mutation } from 'vuex-module-decorators'
 import firebase from "firebase";
 
 // stateFactory: true → Vuex をモジュールモードで扱うために指定
-@Module({ stateFactory: true, namespaced: true, name: 'auth' })
+@Module({ stateFactory: true, namespaced: true, name: 'auths' })
 export default class Auths extends VuexModule {
   private user: firebase.User | null = null
 
